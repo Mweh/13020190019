872751d8a6a111820b8fb447f44c48dc84fe6a4d
Mweh <40236619+Mweh@users.noreply.github.com>
2020-06-26 19:38:12 +0800

    Proyek v.2

---
+++ Proyek/Kasus 1/FahmiKasus1.h
@@ -0,0 +1,15 @@
+// SEBELUM DIJALANKAN PASTIKAN LANG: STANDARD (-std)NYA ADALAH GNU C++ 11 DI CODE GENERATIONS - COMPILER OPTIONS
+// KASUS PERTAMA
+// Saya menggunakan 3 File, yakni 1 file header dan 2 file .cpp
+
+#ifndef FAHMIKASUS1_H 
+#define FAHMIKASUS1_H
+
+int Hitung(int Jumlah[]);
+int Huruf(int Final);
+
+#endif // FAHMIKASUS1_H
+
+// Diatas Ini adalah File Headernya dgn nama file "FahmiKasus1.h"
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------
